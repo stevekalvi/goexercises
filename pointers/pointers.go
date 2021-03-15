6d89d369aa7f6253598767cc3281330823dd1505
Kalvi <kalvisa@upmc.edu>
2021-03-15 21:38:34 +0100

    Answer key for https://tour.golang.org

---
+++ pointers/pointers.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Object struct {
+	Value int
+}
+
+func (o *Object) Double() {
+	o.Value *= 2
+}
+
+func (o *Object) String() string {
+	return (fmt.Sprintf("%d", o.Value))
+}
+
+func main() {
+
+	objs := []*Object{&Object{Value: 1},
+		&Object{Value: 2},
+		&Object{Value: 3},
+		&Object{Value: 4},
+		&Object{Value: 5}}
+	for _, item := range objs {
+		item.Double()
+	}
+
+	fmt.Println(objs)
+}
+go 
